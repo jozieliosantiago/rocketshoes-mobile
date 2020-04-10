cf7932ad70dfb87f181696e1c35e8d766f474283
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-10 13:08:54 -0300

    initial architecture

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Rocketshoes</Text>
+    </View>
+  );
+};
+
+export default App;
