5d36c95a9b4c11e6e256d97ec66069a5953b35bf
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-10 19:45:45 -0300

    home page layout udpate

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import {
+  Container,
+  List,
+  Item,
+  Image,
+  Description,
+  PriceInfo,
+  Button,
+  TextButton,
+  CartIcon,
+  Count,
+} from './styles';
 
 export default function Home() {
   return (
     <Container>
-      <Text>Home</Text>
+      <List
+        horizontal
+        data={['R$ 179,90', 'R$ 178,90', 'R$ 149,90']}
+        keyExtractor={(data) => data}
+        renderItem={({ item }) => (
+          <Item>
+            <Image
+              source={{
+                uri:
+                  'https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe1.jpg?ts=1573040493?ims=280x280',
+              }}
+            />
+            <Description>
+              Tênis Adidas Duramo Lite 2 0 Masculino - Cinza
+            </Description>
+            <PriceInfo>{item}</PriceInfo>
+            <Button>
+              <CartIcon>
+                <Icon name="add-shopping-cart" color="#fff" size={20} />
+                <Count>0</Count>
+              </CartIcon>
+              <TextButton>ADICIONAR</TextButton>
+            </Button>
+          </Item>
+        )}
+      />
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,6 +1,68 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+import { darken } from 'polished';
 
 export const Container = styled.View`
   flex: 1;
+  background: #141419;
+  padding: 10px 10px;
+`;
+export const List = styled.FlatList`
+  height: 400px;
+`;
+
+export const Item = styled.View`
   background: #fff;
+  border-radius: 5px;
+  margin-right: 15px;
+  padding: 10px;
+  width: 250px;
+  height: 440px;
+`;
+
+export const Image = styled.Image`
+  height: 250px;
+  width: 225px;
+  margin-bottom: 10px;
+`;
+
+export const Description = styled.Text`
+  font-size: 18px;
+`;
+
+export const PriceInfo = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  margin: 15px 0;
+`;
+
+export const Button = styled(RectButton)`
+  background: #7159c1;
+  flex-direction: row;
+  height: 50px;
+  border-radius: 4px;
+  align-items: center;
+`;
+
+export const TextButton = styled.Text`
+  color: #fff;
+  flex: 1;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const CartIcon = styled.View`
+  height: 50px;
+  width: 70px;
+  flex-direction: row;
+  align-items: center;
+  background: ${darken(0.03, '#7159c1')};
+  justify-content: center;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+export const Count = styled.Text`
+  margin-left: 10px;
+  color: #fff;
 `;
