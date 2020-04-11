f00e652219642cded9b5bee77432123491fb159c
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-10 23:39:57 -0300

    updated cart page style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,12 +1,89 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import {
+  Container,
+  CartContent,
+  Item,
+  Header,
+  Info,
+  Image,
+  Description,
+  RemoveButton,
+  Price,
+  Amount,
+  Count,
+  Subtotal,
+  Input,
+  Total,
+  Title,
+  Value,
+  SubmitButton,
+  SubmitButtonText,
+  List,
+} from './styles';
 
 export default function Cart() {
+  const products = ['product1', 'product2'];
+
+  const footer = () => {
+    return (
+      <>
+        <Total>
+          <Title>TOTAL</Title>
+          <Value>R$ 539,70</Value>
+        </Total>
+
+        <SubmitButton>
+          <SubmitButtonText>FINALIZAR PEDIDO</SubmitButtonText>
+        </SubmitButton>
+      </>
+    );
+  };
+
   return (
     <Container>
-      <Text>Cart</Text>
+      <CartContent>
+        <List
+          data={products}
+          keyExtractor={(product) => product}
+          renderItem={({ product }) => (
+            <Item key={product}>
+              <Header>
+                <Image
+                  source={{
+                    uri:
+                      'https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe1.jpg?ts=1573040493?ims=280x280',
+                  }}
+                />
+                <Info>
+                  <Description>
+                    Tênis Adidas Duramo Lite 2 0 Masculino - Cinza
+                  </Description>
+                  <Price>R$ 179,90</Price>
+                </Info>
+                <RemoveButton>
+                  <Icon name="delete-forever" color="#7159c1" size={25} />
+                </RemoveButton>
+              </Header>
+
+              <Amount>
+                <Count>
+                  <Icon
+                    name="remove-circle-outline"
+                    size={20}
+                    color="#7159c1"
+                  />
+                  <Input value="0" />
+                  <Icon name="add-circle-outline" size={20} color="#7159c1" />
+                </Count>
+                <Subtotal>R$ 539,70</Subtotal>
+              </Amount>
+            </Item>
+          )}
+          ListFooterComponent={footer()}
+        />
+      </CartContent>
     </Container>
   );
 }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,6 +1,116 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
+  background: #141419;
+  padding: 10px 10px;
+`;
+
+export const CartContent = styled.View`
   background: #fff;
+  padding: 15px 15px;
+  border-radius: 5px;
+`;
+
+export const List = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+})``;
+
+export const Item = styled.View`
+  padding-bottom: 5px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Info = styled.View`
+  width: 160px;
+`;
+
+export const Image = styled.Image`
+  height: 100px;
+  width: 100px;
+`;
+
+export const Description = styled.Text`
+  font-size: 14px;
+`;
+
+export const RemoveButton = styled(RectButton)`
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Price = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+export const Amount = styled.View`
+  height: 50px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: #eee;
+  padding: 0 10px;
+  border-radius: 5px;
+`;
+
+export const Count = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Input = styled.TextInput.attrs({
+  editable: false,
+})`
+  width: 60px;
+  background: #fff;
+  height: 30px;
+  margin: 0 10px;
+  border-radius: 5px;
+  border: solid 1px #ccc;
+  padding: 0;
+  text-align: center;
+  color: #555;
+`;
+
+export const Subtotal = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const Total = styled.View`
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0;
+`;
+
+export const Title = styled.Text`
+  color: #888;
+  font-weight: bold;
+`;
+
+export const Value = styled.Text`
+  font-size: 32px;
+  font-weight: bold;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  height: 40px;
+  background: #7159c1;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  /* margin-bottom: 5px; */
+`;
+
+export const SubmitButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
 `;
