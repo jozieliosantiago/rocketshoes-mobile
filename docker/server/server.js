9215454efdd3bac7948726e67dc29abe4f24d5f2
Joziélio Santiago <jozieliosantiago@gmail.com>
2020-04-12 18:27:06 -0300

    docker server updated

--- docker/server/server.js
+++ docker/server/server.js
@@ -1,9 +1,11 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 
 const port = 3333;
 
 const server = jsonServer.create();
 const db = jsonServer.router('server.json');
+server.use(cors());
 
 server.use('/api', db);
 server.listen(port);
